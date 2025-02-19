3bf4688120a54acc879973a083bb0aeb66d3d5ed
Nitesh Prajapat <niteshp282000@gmail.com>
2025-02-19 23:27:24 +0530

    setting up project

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 import express from 'express';
+import dotenv from 'dotenv'
+import cors from 'cors'
+import cookieParser from 'cookie-parser';
+
+// config
+dotenv.config({});
+
+
+// initialization
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// middlewares
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true }));
+// app.use(cors());
+app.use(cookieParser());
+
+
+
+
+// app home page
+app.get('/', (req, res) => {
+    return res.status(200).json({
+        success: true,
+        message: "This is Home Page for Orra!",
+    });
+});
+
+
+// app listen
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
