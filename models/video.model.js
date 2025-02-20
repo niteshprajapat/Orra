9c6e22a5497403e1f309c6b508727e6f8fa1cf4b
Nitesh Prajapat <niteshp282000@gmail.com>
2025-02-21 00:15:21 +0530

    video model created

---
+++ models/video.model.js
@@ -0,0 +1,66 @@
+import { type } from "express/lib/response";
+import mongoose from "mongoose";
+
+
+const videoSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    thumbnail: {
+        type: String,
+        default: "",
+    },
+    category: {
+        type: String,
+        enum: ['education', 'entertainment', 'gaming', 'vlogs', 'music', 'sports'],
+        required: true,
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    tags: [
+        {
+            type: String,
+        }
+    ],
+    visibility: {
+        type: String,
+        enum: ["public", "private", "unlisted"],
+        default: "public",
+    },
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+    ],
+    dislikes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        }
+    ],
+    views: {
+        type: Number,
+        default: 0,
+    },
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment",
+        }
+    ],
+
+
+
+}, { timestamps: true });
+
+const Video = mongoose.model('Video', videoSchema);
+export default Video;
