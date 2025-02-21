fb22e4c2af3e770291a9a76c5e17eb0b04a4615e
Nitesh Prajapat <niteshp282000@gmail.com>
2025-02-22 00:36:26 +0530

    notification model created

---
+++ models/notification.model.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+
+const notificationSchema = new mongoose.Schema({
+    receiver: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    type: {
+        type: String,
+        enum: ['like', 'comment', 'subscription', 'upload', 'reply'],
+        required: true,
+    },
+    videoId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+    },
+    commentId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+    },
+    isRead: {
+        type: Boolean,
+        default: false,
+    },
+    priority: {
+        type: String,
+        enum: ['high', 'medium', 'low'],
+        default: 'medium'
+    },
+    isDelete: {
+        type: Boolean,
+        default: false,
+    },
+
+
+
+
+}, { timestamps: true });
+
+const Notification = mongoose.model('Notification', notificationSchema);
+export default Notification;
