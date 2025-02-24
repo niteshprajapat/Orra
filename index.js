0ddde3835d743737b615b006d25167a6b34e4d9e
Nitesh Prajapat <niteshp282000@gmail.com>
2025-02-24 23:39:33 +0530

    implemented rate-limiter

--- index.js
+++ index.js
@@ -2,15 +2,32 @@ import express from 'express';
 import dotenv from 'dotenv'
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
+import { connectDB } from './config/database.js';
+import authRoutes from './routes/auth.routes.js'
+import rateLimit from 'express-rate-limit';
 
 // config
 dotenv.config({});
+connectDB();
 
 
 // initialization
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+const limiter = rateLimit({
+    windowMs: 60 * 1000, // 1 min
+    max: 2,
+    message: {
+        success: false,
+        message: "Too many request, please try again later!",
+    },
+    headers: true,
+})
+
+
+app.use(limiter);
 
 // middlewares
 app.use(express.json({ limit: '50mb' }));
@@ -19,13 +36,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 
+// routes middleware
+app.use("/api/v1/auth", authRoutes);
+
 
 
 // app home page
-app.get('/', (req, res) => {
+app.get('/health', (req, res) => {
     return res.status(200).json({
         success: true,
-        message: "This is Home Page for Orra!",
+        message: "Server Up for Orra!",
     });
 });
 
