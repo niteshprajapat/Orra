4544cd30b6796d1642b574e395307a1eccec7267
Nitesh Prajapat <niteshp282000@gmail.com>
2025-02-25 23:13:17 +0530

    authMiddleware and admin middleware created

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,65 @@
+import User from '../models/user.model.js';
+import jwt, { decode } from 'jsonwebtoken';
+
+
+export const isAuthenticated = async (req, res, next) => {
+    try {
+        const token = req.cookies.orra || req.headers.authorization.replace("Bearer ", "");
+        console.log("tokennn", token);
+
+        if (!token) {
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorized! Please login first!",
+            });
+        }
+
+        const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+        console.log("decoded", decoded);
+
+        if (!decoded) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid Token!",
+            });
+        }
+
+        const user = await User.findById(decoded._id);
+        console.log("user", user);
+
+
+        user.password = null;
+        req.user = user;
+
+        next();
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+// isAdmmin
+export const isAdmin = async (req, res, next) => {
+    try {
+        const role = req.user.role;
+        console.log("role", role);
+
+
+        if (role !== "admin") {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid Role",
+            });
+        }
+
+        next();
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Invalid Role",
+        })
+    }
+}
