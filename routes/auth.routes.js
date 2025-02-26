4dfbc1e33ad19d6ee9b8efe5c21542e0e0f15b38
Nitesh Prajapat <niteshp282000@gmail.com>
2025-02-26 21:17:42 +0530

    created sent otp, verify account, resent otp routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, register, logout } from "../controllers/auth.controller.js";
+import { login, register, logout, verifyAccount, resentOtp } from "../controllers/auth.controller.js";
 import { isAdmin, isAuthenticated } from "../middlewares/authMiddleware.js";
 
 
@@ -10,7 +10,9 @@ const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/logout", isAuthenticated, isAdmin, logout);
+router.get("/logout", isAuthenticated, logout);
+router.post("/verify-account", isAuthenticated, verifyAccount);
+router.post("/resend-otp", resentOtp);
 
 
 
