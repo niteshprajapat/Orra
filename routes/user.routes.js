62f412bcb838dd9e61cae9933c95a6ab8eef5b52
Nitesh Prajapat <niteshp282000@gmail.com>
2025-03-01 16:12:11 +0530

    added APIs for update profile, profile, history login devices

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteUserById, getAllDeletedUsers, getAllUsers, getUserById } from '../controllers/user.controller.js';
+import { changeStatus, deleteUserById, getAllDeletedUsers, getAllUsers, getUserById, loginHistory, meProfile, updateProfile } from '../controllers/user.controller.js';
 import { isAdmin, isAuthenticated } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
@@ -8,11 +8,16 @@ const router = express.Router();
 
 router.get("/user/:userId", isAuthenticated, getUserById);
 router.delete("/user/:userId", isAuthenticated, deleteUserById);
+router.get("/me", isAuthenticated, meProfile);
+router.put("/user/updateProfile", isAuthenticated, updateProfile);
+
+router.get("/login-history", isAuthenticated, loginHistory);
 
 
 // Admin Routes
 router.get("/getAllUsers", isAuthenticated, isAdmin, getAllUsers);
 router.get("/getAllDeletedUsers", isAuthenticated, isAdmin, getAllDeletedUsers);
+router.post("/user/change-status/:userId", isAuthenticated, isAdmin, changeStatus);
 
 
 
