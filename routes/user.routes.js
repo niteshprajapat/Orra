f6289f037fe23396aff859bb37075151b45fe74e
Nitesh Prajapat <niteshp282000@gmail.com>
2025-03-02 09:33:19 +0530

    created API routes for subscribe, unsubscribe, request email update, verify email update

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { changeStatus, deleteUserById, getAllDeletedUsers, getAllUsers, getUserById, loginHistory, meProfile, updateProfile } from '../controllers/user.controller.js';
+import { changeStatus, deleteUserById, getAllDeletedUsers, getAllUsers, getUserById, loginHistory, meProfile, requestEmailUpdate, subscribeUser, unSubscribeUser, updateProfile, verifyEmailUpdate } from '../controllers/user.controller.js';
 import { isAdmin, isAuthenticated } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
@@ -11,13 +11,23 @@ router.delete("/user/:userId", isAuthenticated, deleteUserById);
 router.get("/me", isAuthenticated, meProfile);
 router.put("/user/updateProfile", isAuthenticated, updateProfile);
 
+router.post("/user/request-email-update", isAuthenticated, requestEmailUpdate);
+router.post("/user/verify-email-update", verifyEmailUpdate);
+
+// above 2 are pending
+
+
+
 router.get("/login-history", isAuthenticated, loginHistory);
+router.get("/subscribe-user/:userId", isAuthenticated, subscribeUser);
+router.get("/unsubscribe-user/:userId", isAuthenticated, unSubscribeUser);
 
 
 // Admin Routes
 router.get("/getAllUsers", isAuthenticated, isAdmin, getAllUsers);
 router.get("/getAllDeletedUsers", isAuthenticated, isAdmin, getAllDeletedUsers);
 router.post("/user/change-status/:userId", isAuthenticated, isAdmin, changeStatus);
+// router.post("/user/deactive/")
 
 
 
