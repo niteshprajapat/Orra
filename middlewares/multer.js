f7391ae604487de66bb127a622c8a8ebfcd31ef7
Nitesh Prajapat <niteshp282000@gmail.com>
2025-03-05 21:32:41 +0530

    modified multer middleware

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,6 +1,23 @@
-import multer from "multer";
+// import multer from "multer";
 
-const storage = multer.diskStorage({});
+// const storage = multer.diskStorage({});
+// const upload = multer({ storage });
+
+// export default upload;
+
+
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads/"); // Ensure this folder exists
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}-${file.originalname}`);
+    },
+});
+
 const upload = multer({ storage });
 
 export default upload;
