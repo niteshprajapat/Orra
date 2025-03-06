c71eb844efcbaa87efe2768acffefaeee0b2d37a
Nitesh Prajapat <niteshp282000@gmail.com>
2025-03-06 23:53:00 +0530

    implemented google auth api routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,4 +1,6 @@
 import express from "express";
+import passport from "passport";
+import jwt from "jsonwebtoken";
 import { login, register, logout, verifyAccount, resentOtp, forgotPassword, resetPassword, changePassword } from "../controllers/auth.controller.js";
 import { isAdmin, isAuthenticated } from "../middlewares/authMiddleware.js";
 
@@ -18,5 +20,24 @@ router.post("/resend-otp", resentOtp);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
 router.post("/change-password", isAuthenticated, changePassword);
+
+
+// Google OAuth Routes
+router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+
+router.get(
+    "/google/callback",
+    passport.authenticate("google", { failureRedirect: "http://localhost:3000/login", session: false }),
+    (req, res) => {
+        const user = req.user;
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "7d",
+        });
+
+        // res.redirect(`http://localhost:3000?token=${token}`);
+        res.redirect(`http://localhost:5000?token=${token}`);
+    }
+);
+
 
 export default router;
