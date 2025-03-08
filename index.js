f8761d8de3e7ffcacb7ebecf5b5c289a245a178b
Nitesh Prajapat <niteshp282000@gmail.com>
2025-03-08 13:57:50 +0530

    added new config for routes, google oauth

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ import express from 'express';
 import dotenv from 'dotenv'
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import useragent from 'express-useragent';
+import passport from 'passport';
 import { connectDB } from './config/database.js';
-import authRoutes from './routes/auth.routes.js'
 import rateLimit from 'express-rate-limit';
+import authRoutes from './routes/auth.routes.js';
+import userRoutes from './routes/user.routes.js';
+import videoRoutes from './routes/video.routes.js';
+import './config/passport.js';
 
 // config
 dotenv.config({});
@@ -18,7 +24,7 @@ const port = process.env.PORT || 5000;
 
 const limiter = rateLimit({
     windowMs: 60 * 1000, // 1 min
-    max: 2,
+    max: 10,
     message: {
         success: false,
         message: "Too many request, please try again later!",
@@ -34,10 +40,15 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 // app.use(cors());
 app.use(cookieParser());
-
+app.use(useragent.express());
+app.use(session({ secret: process.env.JWT_SECRET, resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // routes middleware
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/videos", videoRoutes);
 
 
 
