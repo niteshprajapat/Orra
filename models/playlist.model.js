aec1ecb01636b865185a7b303499e454664612ff
Nitesh Prajapat <niteshp282000@gmail.com>
2025-03-08 18:31:53 +0530

    playlist model modified

--- models/playlist.model.js
+++ models/playlist.model.js
@@ -10,20 +10,43 @@ const playlistSchema = new mongoose.Schema({
         {
             type: mongoose.Schema.Types.ObjectId,
             ref: "Video",
+            unique: true,
         },
     ],
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    thumbnail: {
+        url: {
+            type: String,
+            default: "",
+        },
+        public_id: {
+            type: String,
+            default: "",
+        },
+    },
     isDelete: {
         type: Boolean,
         default: false,
     },
-    isPublic: {
-        type: Boolean,
-        default: true,
+    visibility: {
+        type: String,
+        enum: ["public", "private", "unlisted"],
+        default: "public",
     },
     views: {
         type: Number,
         default: 0,
     },
+    viewedBy: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+    ],
     likes: [
         {
             type: mongoose.Schema.Types.ObjectId,
