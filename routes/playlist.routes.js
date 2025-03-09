1b101ca687a42a1fb44f79ef3c8181e4be25d78f
Nitesh Prajapat <niteshp282000@gmail.com>
2025-03-09 23:29:00 +0530

    APIs created for getAllPlaylistUser, getPublicPlaylist, searchPlaylist, update and change visibility of playlist status

--- routes/playlist.routes.js
+++ routes/playlist.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { isAdmin, isAuthenticated } from "../middlewares/authMiddleware.js";
-import { addVideoInPlaylist, createPlaylist, deletePlaylistThumbnail, deleteVideoFromPlaylist, getAllPlaylistsByUserId, getPlaylistById, uploadPlaylistThumbnail } from "../controllers/playlist.controller.js";
+import { addVideoInPlaylist, changeVisibilityofPlaylistById, createPlaylist, deletePlaylistById, deletePlaylistThumbnail, deleteVideoFromPlaylist, getAllPlaylistsByUserId, getAllPublicPlaylists, getPlaylistById, searchPlaylists, updatePlaylistById, uploadPlaylistThumbnail } from "../controllers/playlist.controller.js";
 import upload from '../middlewares/multer.js';
 
 const router = express.Router();
@@ -14,7 +14,15 @@ router.put("/add-video-in-playlist/:playlistId/video/:videoId", isAuthenticated,
 router.delete("/delete-video-from-playlist/:playlistId/video/:videoId", isAuthenticated, deleteVideoFromPlaylist);
 router.get("/get-playlist-by-id/:playlistId", isAuthenticated, getPlaylistById);
 router.get("/get-all-playlist/:userId", isAuthenticated, getAllPlaylistsByUserId);
+router.get("/get-all-public-playlist", isAuthenticated, getAllPublicPlaylists);
 
+router.delete("/delete-playlist/:playlistId", isAuthenticated, deletePlaylistById);
+
+router.put("/change-visibility-of-playlist/:playlistId", isAuthenticated, changeVisibilityofPlaylistById);
+router.put("/update-playlist/:playlistId", isAuthenticated, updatePlaylistById);
+
+// searchPlaylists
+router.get("/search-public", isAuthenticated, searchPlaylists);
 
 
 
