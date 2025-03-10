215265dd2f3cf7a85fd5597fd60f984483d35dd3
Nitesh Prajapat <niteshp282000@gmail.com>
2025-03-10 21:54:54 +0530

    added Github OAuth using passport

--- config/passport.js
+++ config/passport.js
@@ -1,11 +1,14 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import { Strategy as GithubStrategy } from 'passport-github2';
 import dotenv from 'dotenv';
 import User from "../models/user.model.js";
 
 
 dotenv.config();
 
+
+// Google OAuth
 passport.use(
     new GoogleStrategy(
         {
@@ -44,6 +47,47 @@ passport.use(
     )
 );
 
+
+// Github OAuth
+passport.use(
+    new GithubStrategy(
+        {
+            clientID: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            callbackURL: "http://localhost:5000/api/v1/auth/github/callback",
+            scope: ["user:email"],
+        },
+        async (accessToken, refreshToken, profile, done) => {
+            try {
+                console.log("Github profile", profile);
+
+                let user = await User.findOne({ githubId: profile.id });
+
+                if (!user) {
+                    user = await User.create({
+                        githubId: profile.id,
+                        fullname: profile.displayName || profile.username,
+                        username: profile.username,
+                        email: profile.emails?.[0]?.value || "",
+                        profilePicture: {
+                            url: profile.photos[0]?.value || "",
+                            // public_id
+                        },
+                        isVerified: true,
+                    });
+
+                    await user.save();
+                }
+
+                return done(null, user);
+
+            } catch (error) {
+                return done(error, null);
+            }
+        }
+
+    )
+)
 
 
 passport.serializeUser((user, done) => {
