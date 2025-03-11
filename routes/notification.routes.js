639cae17415925557b61d0280078d3fae8a58272
Nitesh Prajapat <niteshp282000@gmail.com>
2025-03-11 23:54:40 +0530

    created all required API routes for notifications

--- routes/notification.routes.js
+++ routes/notification.routes.js
@@ -1,12 +1,18 @@
 import express from "express";
 import { isAdmin, isAuthenticated } from "../middlewares/authMiddleware.js";
-import { getAllAdminNotifications, getAllUserNotifications, markNotificationAsRead } from "../controllers/notification.controller.js";
+import { deleteAllNotification, deleteNotificationById, getAllAdminNotifications, getAllUnreadUserNotifications, getAllUserNotifications, getNotificationByType, markAllNotificationAsRead, markNotificationAsRead } from "../controllers/notification.controller.js";
 
 const router = express.Router();
 
 router.get("/get-all-admin-notifications", isAuthenticated, isAdmin, getAllAdminNotifications);
 router.get("/get-all-user-notifications", isAuthenticated, getAllUserNotifications);
+router.get("/get-all-unread-user-notifications", isAuthenticated, getAllUnreadUserNotifications);
 router.get("/mark-as-read-notification/:notificationId", isAuthenticated, markNotificationAsRead);
+router.get("/mark-all-notification-as-read", isAuthenticated, markAllNotificationAsRead);
 
+router.delete("/delete-all-notification", isAuthenticated, deleteAllNotification);  //  soft delete
+router.delete("/delete-notification/:notificationId", isAuthenticated, deleteNotificationById); //  soft delete
+
+router.get("/get-notifications-by-type/:type", isAuthenticated, getNotificationByType);
 
 export default router;
