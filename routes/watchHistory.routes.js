c12d33c00dc67664ad82a9c942b96e1698f3c9fd
Nitesh Prajapat <niteshp282000@gmail.com>
2025-03-17 22:44:44 +0530

    working on api user watch history functionality

--- routes/watchHistory.routes.js
+++ routes/watchHistory.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { isAdmin, isAuthenticated } from "../middlewares/authMiddleware.js";
-import { getUserWatchHistory } from "../controllers/watchHistory.controller.js";
+import { clearWatchHistory, getUserWatchHistory, removeFromWatchHistory } from "../controllers/watchHistory.controller.js";
 
 const router = express.Router();
 
 router.get("/get-user-watch-history", isAuthenticated, getUserWatchHistory);
+router.delete("/clear-watch-history", isAuthenticated, clearWatchHistory);
+router.delete("/remove-from-watch-history/:videoId", isAuthenticated, removeFromWatchHistory);
 
 export default router;
