40c27c12ad82487424e362b71108556fb3613ccf
Nitesh Prajapat <niteshp282000@gmail.com>
2025-03-17 22:44:18 +0530

    working on user watch history functionality

--- controllers/watchHistory.controller.js
+++ controllers/watchHistory.controller.js
@@ -39,3 +39,78 @@ export const getUserWatchHistory = async (req, res) => {
         });
     }
 }
+
+// clearWatchHistory
+export const clearWatchHistory = async (req, res) => {
+    try {
+        const userId = req.user._id;
+
+        const userWatchHistory = await WatchHistory.findOne({ userId: userId });
+
+        if (!userWatchHistory) {
+            return res.status(404).json({
+                success: false,
+                message: "Watch history not found.",
+            });
+        }
+
+        userWatchHistory.videos = [];
+
+        await userWatchHistory.save();
+
+        return res.status(200).json({
+            success: true,
+            message: "Watch history cleared successfully.",
+        });
+
+
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Error in deleteThumbnail API!",
+        });
+    }
+}
+// removeFromWatchHistory
+export const removeFromWatchHistory = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const videoId = req.params.videoId;
+
+        const userWatchHistory = await WatchHistory.findOne({ userId: userId });
+
+        if (!userWatchHistory) {
+            return res.status(404).json({
+                success: false,
+                message: "Watch history not found.",
+            });
+        }
+
+
+
+        const videoIndex = userWatchHistory.videos.findIndex((video) => video.videoId.toString() === videoId.toString());
+
+        if (videoIndex !== -1) {
+            userWatchHistory.videos[videoIndex].isDeleted = true;
+            await userWatchHistory.save();
+        }
+
+        await userWatchHistory.save();
+
+        return res.status(200).json({
+            success: true,
+            message: "Video removed from watch history.",
+        });
+
+
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Error in deleteThumbnail API!",
+        });
+    }
+}
