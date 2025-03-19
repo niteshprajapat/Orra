fc8fb5ba72ea3a7d0dce1704a1799a74d800bd59
Nitesh Prajapat <niteshp282000@gmail.com>
2025-03-19 23:10:47 +0530

    added subscription model

---
+++ models/subscription.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    stripeSubscriptionId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    status: {
+        type: String,
+        enum: ["active", "canceled", "expired"],
+        default: "active",
+    },
+    currentPeriodStart: {
+        type: Date,
+        required: true,
+    },
+    currentPeriodEnd: {
+        type: Date,
+        required: true,
+    },
+}, { timestamps: true });
+
+const Subscription = mongoose.model("Subscription", subscriptionSchema);
+export default Subscription;
