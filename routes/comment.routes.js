08e17433b10c4d118e4d67ba21689349bc06a6cc
Nitesh Prajapat <niteshp282000@gmail.com>
2025-03-24 22:05:36 +0530

    working on comment api routes

--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { isAdmin, isAuthenticated } from "../middlewares/authMiddleware.js";
 import { deleteAllNotification, deleteNotificationById, getAllAdminNotifications, getAllUnreadUserNotifications, getAllUserNotifications, getNotificationByType, markAllNotificationAsRead, markNotificationAsRead } from "../controllers/notification.controller.js";
-import { createComment, getCommentByCommentId, getCommentByVideoId, updateCommentByCommentId } from "../controllers/comment.controller.js";
+import { createComment, deleteCommentByCommentId, getCommentByCommentId, getCommentByVideoId, updateCommentByCommentId } from "../controllers/comment.controller.js";
 
 const router = express.Router();
 
@@ -10,5 +10,6 @@ router.post("/create-comment/:videoId", isAuthenticated, createComment);
 router.get("/get-comments/:videoId", isAuthenticated, getCommentByVideoId);
 router.get("/get-comment/:commentId", isAuthenticated, getCommentByCommentId);
 router.put("/update-comment/:commentId", isAuthenticated, updateCommentByCommentId);
+router.delete("/delete-comment/:commentId", isAuthenticated, deleteCommentByCommentId);
 
 export default router;
