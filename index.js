055e7d1a7aa0bb46f4e9a712cd87f8cc1fce502d
Nitesh Prajapat <niteshp282000@gmail.com>
2025-03-25 22:59:39 +0530

    added comment routes

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ import playlistRoutes from './routes/playlist.routes.js';
 import notificationRoutes from './routes/notification.routes.js';
 import paymentRoutes from './routes/payment.routes.js';
 import watchHistoryRoutes from './routes/watchHistory.routes.js';
+import commentRoutes from './routes/comment.routes.js';
 import './config/passport.js';
 
 
@@ -28,10 +29,6 @@ import './config/passport.js';
  * 
  * 
  */
-
-
-
-
 
 
 // config
@@ -76,6 +73,7 @@ app.use("/api/v1/notifications", notificationRoutes);
 app.use("/api/v1/notifications", notificationRoutes);
 app.use("/api/v1/watchHistory", watchHistoryRoutes);
 app.use("/api/v1/payment", paymentRoutes);
+app.use("/api/v1/comments", commentRoutes);
 
 
 
