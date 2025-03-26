f3f73968294af9424452e64d1492a11b5abd2990
Nitesh Prajapat <niteshp282000@gmail.com>
2025-03-26 22:36:31 +0530

    created comment api routes

--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { isAdmin, isAuthenticated } from "../middlewares/authMiddleware.js";
 import { deleteAllNotification, deleteNotificationById, getAllAdminNotifications, getAllUnreadUserNotifications, getAllUserNotifications, getNotificationByType, markAllNotificationAsRead, markNotificationAsRead } from "../controllers/notification.controller.js";
-import { createComment, deleteCommentByCommentId, getCommentByCommentId, getCommentByVideoId, updateCommentByCommentId } from "../controllers/comment.controller.js";
+import { createComment, deleteCommentByCommentId, dislikeUndislikeCommentByCommentId, getAllCommentsByUser, getCommentByCommentId, getCommentByVideoId, likeUnlikeCommentByCommentId, updateCommentByCommentId } from "../controllers/comment.controller.js";
 
 const router = express.Router();
 
@@ -11,5 +11,8 @@ router.get("/get-comments/:videoId", isAuthenticated, getCommentByVideoId);
 router.get("/get-comment/:commentId", isAuthenticated, getCommentByCommentId);
 router.put("/update-comment/:commentId", isAuthenticated, updateCommentByCommentId);
 router.delete("/delete-comment/:commentId", isAuthenticated, deleteCommentByCommentId);
+router.get("/like-unlike-comment/:commentId", isAuthenticated, likeUnlikeCommentByCommentId);
+router.get("/dislike-undislike-comment/:commentId", isAuthenticated, dislikeUndislikeCommentByCommentId);
+router.get("/get-all-comments-by-user/:userId", isAuthenticated, getAllCommentsByUser);
 
 export default router;
