368a5b2d1a127232ca9fa9fd1eb969537781b015
Nitesh Prajapat <niteshp282000@gmail.com>
2025-03-30 17:08:42 +0530

    added redis config

---
+++ config/redis.js
@@ -0,0 +1,23 @@
+import redis from 'redis';
+import dotenv from 'dotenv';
+
+
+export const redisClient = redis.createClient({
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: "redis-10910.c99.us-east-1-4.ec2.redns.redis-cloud.com",
+        port: 10910,
+    }
+});
+
+
+export const connectToRedis = async () => {
+    try {
+        await redisClient.connect();
+        console.log("Connected to Redis!!");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
