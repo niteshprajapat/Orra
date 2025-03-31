2977ad2b6b912720c75a38fad9f2e4dcd0416271
Nitesh Prajapat <niteshp282000@gmail.com>
2025-03-31 19:22:37 +0530

    added API routes for chat related functionalities like add user, remove, delete

--- routes/chat.routes.js
+++ routes/chat.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { isAdmin, isAuthenticated } from "../middlewares/authMiddleware.js";
-import { createGroupChat, createOneToOneChat, sendMessage } from "../controllers/chat.controller.js";
+import { addParticipantToGroup, createGroupChat, createOneToOneChat, deleteGroupChat, deleteMessage, deleteOneToOneChat, getChatMessages, markMessageRead, removeUserFromGroup, sendMessage } from "../controllers/chat.controller.js";
 
 const router = express.Router();
 
@@ -8,7 +8,14 @@ const router = express.Router();
 router.post("/chat/one-to-one", isAuthenticated, createOneToOneChat);
 router.post("/chat/group-chat", isAuthenticated, createGroupChat);
 router.post("/chat/sendMessage", isAuthenticated, sendMessage);
+router.get("/chat/getChatMessages/:chatId", isAuthenticated, getChatMessages);
+router.get("/message-read/:messageId", isAuthenticated, markMessageRead);
+router.put("/chat/group/add", isAuthenticated, addParticipantToGroup);
+router.delete("/chat/delete-group-chat/:chatId", isAuthenticated, deleteGroupChat);
 
+router.delete("/chat/delete-message/:messageId", isAuthenticated, deleteMessage);
+router.delete("/chat/delete-one-to-one-chat/:chatId", isAuthenticated, deleteOneToOneChat);
+router.delete("/chat/group/remove-user/:chatId", isAuthenticated, removeUserFromGroup);
 
 
 export default router;
