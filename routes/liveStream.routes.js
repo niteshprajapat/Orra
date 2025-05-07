38963e69be53b05356fbe04268f1b691ba330037
Nitesh Prajapat <niteshp282000@gmail.com>
2025-05-07 23:37:48 +0530

    API routes created for endLiveStream and getAllActiveLiveStreams

--- routes/liveStream.routes.js
+++ routes/liveStream.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { isAdmin, isAuthenticated } from "../middlewares/authMiddleware.js";
-import { startLiveStream } from "../controllers/liveStream.controller.js";
+import { endLiveStream, startLiveStream } from "../controllers/liveStream.controller.js";
 import upload from '../middlewares/multer.js';
 
 const router = express.Router();
@@ -8,5 +8,7 @@ const router = express.Router();
 
 
 router.post("/start-live-stream", isAuthenticated, upload.single("file"), startLiveStream);
+router.get("/end-live-stream", isAuthenticated, endLiveStream);
+router.get("get-active-live-stream", isAuthenticated, getActiveLiveStreams);
 
 export default router;
